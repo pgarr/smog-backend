2dff299eb819284341c19c515a93d883217db273
pgarr <garlej.p@gmail.com>
2020-03-12 20:33:33 +0100

    logi podzielone na osobne pliki dla osobnych modułów

--- app/__init__.py
+++ app/__init__.py
@@ -26,8 +26,11 @@ def create_app(config_class=Config):
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
+    # loggers
+    module_loggers = ['sqlalchemy', 'gios_api']
+
     # mail errors
-    if not app.debug and not app.testing:
+    if not app.debug and not app.testing:  # TODO: odkomplikować tworzenie logów. Strategy/factory/builder pattern?
         if app.config['MAIL_SERVER']:
             auth = None
             if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -41,31 +44,40 @@ def create_app(config_class=Config):
                 toaddrs=app.config['ADMINS'], subject='smog-backend failure',
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
 
-        # app.logger
+            app.logger.addHandler(mail_handler)
+            for logger_name in module_loggers:
+                logger = logging.getLogger(logger_name)
+                logger.addHandler(mail_handler)
+
+        # stdout loggers
         if app.config['LOG_TO_STDOUT']:
-            handler = logging.StreamHandler()
+            stdout_handler = logging.StreamHandler()
+            stdout_handler.setLevel(logging.INFO)
+
+            app.logger.addHandler(stdout_handler)
+            for logger_name in module_loggers:
+                logger = logging.getLogger(logger_name)
+                logger.addHandler(stdout_handler)
+
+        # file loggers
         else:
+            formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            handler = RotatingFileHandler('logs/smog-api.log', maxBytes=102400, backupCount=100)
-            handler.setFormatter(
-                logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        handler.setLevel(logging.INFO)
-        app.logger.addHandler(handler)
-        for logger in (  # TODO: podzielić loggery na osobne pliki - teraz jest nieczytelnie, a będzie jeszcze bardziej
-                app.logger,
-                logging.getLogger('sqlalchemy'),
-                logging.getLogger('gios_api'),
-        ):
-            logger.addHandler(handler)
-            try:
-                logger.addHandler(mail_handler)
-            except NameError:
-                pass
+
+            app_handler = RotatingFileHandler('logs/smog-api.log', maxBytes=102400, backupCount=100)
+            app_handler.setFormatter(formatter)
+            app.logger.addHandler(app_handler)
+
+            for logger_name in module_loggers:
+                module_handler = RotatingFileHandler('logs/' + logger_name + '.log', maxBytes=102400, backupCount=100)
+                module_handler.setFormatter(formatter)
+                logger = logging.getLogger(logger_name)
+                logger.addHandler(module_handler)
 
         app.logger.setLevel(logging.INFO)
+
         app.logger.info('smog-backend startup')
 
     return app
