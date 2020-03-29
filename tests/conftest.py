941e74415d46dce634fe0faa87e40332ea03fe48
pgarr <garlej.p@gmail.com>
2020-03-29 13:50:57 +0200

    testy endpointa register

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 
 from app import create_app, db
+from app.models import Subscription, SubscriptionHour
 from tests.utils import TestConfig
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def test_client():
     # set up
     flask_app = create_app(TestConfig)
@@ -20,8 +21,8 @@ def test_client():
     ctx.pop()
 
 
-@pytest.fixture(scope='module')
-def init_database():
+@pytest.fixture
+def database():
     # set up
     db.create_all()
 
@@ -30,3 +31,17 @@ def init_database():
     # tear down
     db.session.remove()
     db.drop_all()
+
+
+@pytest.fixture
+def make_subscription(database):
+    def _make_sub(email, lat, lon, hours):
+        sub = Subscription(email=email, lat=lat, lon=lon)
+        sub.hours = [SubscriptionHour(hour=h) for h in hours]
+
+        database.session.add(sub)
+        database.session.commit()
+
+        return sub
+
+    return _make_sub
--- tests/functional/test_request.py
+++ tests/functional/test_request.py
@@ -1,4 +1,109 @@
+import pytest
+
+from app.models import Subscription
+
+
 def test_connection(test_client):
     response = test_client.get('/api/')
     assert response.status_code == 200
-    assert b'{"message":"API is online!"}' in response.data
+
+
+def test_register(test_client, database):
+    email = "sample@test.pl"
+    lat = 51.1234
+    lon = 21.0101
+    hours = [12, 14, 20]
+
+    response = test_client.post('/api/register',
+                                json={"email": email, "lat": lat, "lon": lon, "hours": hours})
+    assert response.status_code == 200
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 1
+
+    sub = subscriptions[0]
+    assert sub.email == email
+    assert sub.lat == lat
+    assert sub.lon == lon
+    assert sub.get_int_hours() == hours
+
+
+def test_register_no_data(test_client, database):
+    response = test_client.post('/api/register')
+    assert response.status_code == 400
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 0
+
+
+def test_register_email_occupied(test_client, database, make_subscription):
+    email = "sample@test.pl"
+    lat = 51.1234
+    lon = 21.0101
+    hours = [12, 14, 20]
+
+    sub = make_subscription(email, lat, lon, hours)
+
+    response = test_client.post('/api/register',
+                                json={"email": email, "lat": lat, "lon": lon, "hours": hours})
+
+    assert response.status_code == 422
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 1
+
+
+def test_register_no_email(test_client, database):
+    lat = 51.1234
+    lon = 21.0101
+    hours = [12, 14, 20]
+
+    response = test_client.post('/api/register',
+                                json={"lat": lat, "lon": lon, "hours": hours})
+
+    assert response.status_code == 422
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 0
+
+
+def test_register_no_lat(test_client, database):
+    email = "sample@test.pl"
+    lon = 21.0101
+    hours = [12, 14, 20]
+
+    response = test_client.post('/api/register',
+                                json={"email": email, "lon": lon, "hours": hours})
+
+    assert response.status_code == 422
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 0
+
+
+def test_register_no_lon(test_client, database):
+    email = "sample@test.pl"
+    lat = 51.1234
+    hours = [12, 14, 20]
+
+    response = test_client.post('/api/register',
+                                json={"email": email, "lat": lat, "hours": hours})
+
+    assert response.status_code == 422
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 0
+
+
+def test_register_no_hours(test_client, database):
+    email = "sample@test.pl"
+    lat = 51.1234
+    lon = 21.0101
+
+    response = test_client.post('/api/register',
+                                json={"email": email, "lat": lat, "lon": lon})
+
+    assert response.status_code == 422
+
+    subscriptions = Subscription.query.all()
+    assert len(subscriptions) == 0
