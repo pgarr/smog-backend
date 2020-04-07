09b72b9bd78ee4fea1334370bf8ba12f83d73b4c
pgarr <garlej.p@gmail.com>
2020-04-07 19:28:13 +0200

    wydzielenie tworzenia loggerów do osobnych funkcji

--- app/__init__.py
+++ app/__init__.py
@@ -29,58 +29,72 @@ def create_app(config_class=Config):
     # loggers
     module_loggers = ['sqlalchemy', 'gios_api', 'notifier']
 
-    # mail errors
-    if not app.debug and not app.testing:  # TODO: odkomplikować tworzenie logów. Strategy/factory/builder pattern?
+    if not app.debug and not app.testing:  # TODO: dalej popracować nad tworzeniem logów. Wciąż zduplikowany kod
+        # mail errors
         if app.config['MAIL_SERVER']:
-            auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            secure = None
-            if app.config['MAIL_USE_TLS']:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='smog-backend failure',
-                credentials=auth, secure=secure)
-            mail_handler.setLevel(logging.ERROR)
-
-            app.logger.addHandler(mail_handler)
-            for logger_name in module_loggers:
-                logger = logging.getLogger(logger_name)
-                logger.addHandler(mail_handler)
+            handler = set_stmp_handler(app.config, level=logging.ERROR)
+            register_handler(app, module_loggers, handler)
 
         # stdout loggers
         if app.config['LOG_TO_STDOUT']:
-            stdout_handler = logging.StreamHandler()
-            stdout_handler.setLevel(logging.INFO)
-
-            app.logger.addHandler(stdout_handler)
-            for logger_name in module_loggers:
-                logger = logging.getLogger(logger_name)
-                logger.addHandler(stdout_handler)
+            handler = set_stdout_logger(level=logging.INFO)
+            register_handler(app, module_loggers, handler)
 
         # file loggers
         else:
-            formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-
-            app_handler = RotatingFileHandler('logs/smog-api.log', maxBytes=102400, backupCount=100)
-            app_handler.setFormatter(formatter)
-            app.logger.addHandler(app_handler)
-
-            for logger_name in module_loggers:
-                module_handler = RotatingFileHandler('logs/' + logger_name + '.log', maxBytes=102400, backupCount=100)
-                module_handler.setFormatter(formatter)
-                logger = logging.getLogger(logger_name)
-                logger.addHandler(module_handler)
+            register_file_loggers(app, module_loggers)
 
         app.logger.setLevel(logging.INFO)
 
         app.logger.info('smog-backend startup')
 
     return app
+
+
+def set_stmp_handler(config, level=logging.ERROR):
+    auth = None
+    if config['MAIL_USERNAME'] or config['MAIL_PASSWORD']:
+        auth = (config['MAIL_USERNAME'], config['MAIL_PASSWORD'])
+    secure = None
+    if config['MAIL_USE_TLS']:
+        secure = ()
+    handler = SMTPHandler(
+        mailhost=(config['MAIL_SERVER'], config['MAIL_PORT']),
+        fromaddr='no-reply@' + config['MAIL_SERVER'],
+        toaddrs=config['ADMINS'], subject='smog-backend failure',
+        credentials=auth, secure=secure)
+    handler.setLevel(level)
+
+    return handler
+
+
+def set_stdout_logger(level=logging.INFO):
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    return handler
+
+
+def register_handler(app, module_loggers, handler):
+    app.logger.addHandler(handler)
+    for logger_name in module_loggers:
+        logger = logging.getLogger(logger_name)
+        logger.addHandler(handler)
+
+
+def register_file_loggers(app, module_loggers):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    app_handler = RotatingFileHandler('logs/smog-api.log', maxBytes=102400, backupCount=100)
+    app_handler.setFormatter(formatter)
+
+    app.logger.addHandler(app_handler)
+    for logger_name in module_loggers:
+        module_handler = RotatingFileHandler('logs/' + logger_name + '.log', maxBytes=102400, backupCount=100)
+        module_handler.setFormatter(formatter)
+        logger = logging.getLogger(logger_name)
+        logger.addHandler(module_handler)
 
 
 from app import models
