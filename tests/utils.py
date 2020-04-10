1b6798f22c998c91570b7875f4b4acca426ee589
pgarr <garlej.p@gmail.com>
2020-04-10 15:41:17 +0200

    Konfiguracja do testów przeciąża dane serwera mailowego, by nie wysyłać przypadkowych maili podczas testowania

--- tests/utils.py
+++ tests/utils.py
@@ -4,3 +4,8 @@ from config import Config
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+    MAIL_SERVER = None
+    MAIL_PORT = None
+    MAIL_USERNAME = None
+    MAIL_PASSWORD = None
